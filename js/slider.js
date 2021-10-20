8cba3b711d0ab93fcbab62cab1b29628b0346954
zinio3o <92780218+zinio3o@users.noreply.github.com>
2021-10-20 11:24:38 +0900

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,22 +1,19 @@
-//slider.js
-
+// slider.js
 $(function(){
-    var imgNum = 0; //이미지 번호
-    //초기화
+    var imgNum = 0; // 이미지 번호
+    // 초기화
     $('#main-visual .slider img')
-        .hide()
-        .eq(0)
-        .show();
-        //이미지 자동전환
+      .hide()
+      .eq(0)
+      .show();
+    // 이미지 자동 전환
     setInterval(function(){
-    imgNum++; //이미지 증가
-        if(imgNum > 2) imgNum = 0;
-        $('#main-visual .slider img')
-        .fadeOut()
-        .eq(imgNum)
-        .fadeIn();
-    },5000)
-    
-})
+      imgNum++; // 이미지 증가
+      if(imgNum > 2) imgNum = 0;
+      $('#main-visual .slider img')
+      .fadeOut()
+      .eq(imgNum)
+      .fadeIn();      
+    }, 5000);
 
-
+});
